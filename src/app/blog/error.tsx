6a4390b8b393990639d05e8eb55d95e511e4ea9b
Самудинова <azicakcl@gmail.com>
2024-03-03 23:50:32 +0600

    removed blog page

--- src/app/blog/error.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-const ErrorWrapper = ({ error }: { error: Error }) => {
-  return <h1>{error.message}</h1>;
-};
-export default ErrorWrapper;
--- src/app/blog/loading.tsx
+++
@@ -1,5 +0,0 @@
-const LoadingPost = () => {
-  return <h1>Loading...</h1>;
-};
-
-export default LoadingPost;
