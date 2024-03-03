fda64e0046c0f284a0d6825eb3d1154e61bbdf99
Самудинова <azicakcl@gmail.com>
2024-03-03 23:36:47 +0600

    no comment

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // swcMinify: true,
-  // async rewrites(){
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       destination: 
-  //     }
-  //   ]
-  // },
   images: {
     remotePatterns: [
       {
